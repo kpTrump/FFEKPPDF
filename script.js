d439228dfa3c4b72cc1397d2836802e208d34375
kpTrump <konradas.peciulis@trumphotels.com>
2024-12-06 10:34:37 -0600

    Creating test folder

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const pdfParse = require('pdf-parse');
 const xlsx = require('xlsx');
 
 // Path to the folder containing PDF files
-const pdfFolderPath = './\tchfileserver\TCH-Accounting\CONDO\Statements\2024 hotel units statements\11_Nov_24\OWN';
+const pdfFolderPath = 'C:\Users\konradas.peciulis\Desktop\Empty Folder';
 
 // Read all PDF files in the directory
 fs.readdir(pdfFolderPath, (err, files) => {
